770b6e81bd694a673576e90e7b1c3473325f13ea
rentoumacsj <445878453@qq.com>
2017-09-22 20:20:08 +0800

    First commit.

---
+++ app/src/main/java/com/raywenderlich/adaptiveweather/MainActivity.java
@@ -0,0 +1,134 @@
+package com.raywenderlich.adaptiveweather;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.google.android.flexbox.FlexboxLayout;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+  private List<Location> mLocations = new ArrayList<>();
+  LocationAdapter mLocationAdapter;
+
+  private static final String SELECTED_LOCATION_INDEX = "selectedLocationIndex";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.list);
+    mRecyclerView.setHasFixedSize(true);
+
+    LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+    DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, layoutManager.getOrientation());
+    mRecyclerView.addItemDecoration(dividerItemDecoration);
+    mRecyclerView.setLayoutManager(layoutManager);
+
+    loadData();
+
+    mLocationAdapter = new LocationAdapter(this, mLocations, new LocationAdapter.OnItemClickListener() {
+      @Override
+      public void onItemClick(Location location) {
+        loadForecast(location.getForecast());
+      }
+    });
+    mRecyclerView.setAdapter(mLocationAdapter);
+
+    if(savedInstanceState != null){
+      //设置转屏之后选中的状态
+      int index = savedInstanceState.getInt(SELECTED_LOCATION_INDEX);
+      mLocationAdapter.setSelectedLocationIndex(index);
+      loadForecast(mLocations.get(index).getForecast());
+    }
+  }
+
+  private void loadData() {
+    String json = null;
+    try {
+      InputStream is = getAssets().open("data.json");
+      int size = is.available();
+      byte[] buffer = new byte[size];
+      is.read(buffer);
+      is.close();
+      json = new String(buffer, "UTF-8");
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    JSONArray array = null;
+    try {
+      array = new JSONArray(json);
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    for (int i = 0; i < array.length(); i++) {
+      try {
+        JSONObject object = (JSONObject) array.get(i);
+        JSONArray stringArray = (JSONArray) object.get("forecast");
+        List<String> forecast = new ArrayList<String>();
+        for (int j = 0; j < stringArray.length(); j++) {
+          forecast.add(stringArray.getString(j));
+        }
+        Location location = new Location((String) object.get("name"), forecast);
+        mLocations.add(location);
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private Drawable mapWeatherToDrawable(String forecast){
+    int drawableId = 0;
+    switch (forecast){
+      case "sun":
+        drawableId = R.drawable.ic_sun;
+        break;
+      case "rain":
+        drawableId = R.drawable.ic_rain;
+        break;
+      case "fog":
+        drawableId = R.drawable.ic_fog;
+        break;
+      case "thunder":
+        drawableId = R.drawable.ic_thunder;
+        break;
+      case "cloud":
+        drawableId = R.drawable.ic_cloud;
+        break;
+      case "snow":
+        drawableId = R.drawable.ic_snow;
+        break;
+    }
+    return getResources().getDrawable(drawableId);
+  }
+
+  private void loadForecast(List<String> forecast){
+    FlexboxLayout forecastView = (FlexboxLayout) findViewById(R.id.forecast);
+    for(int i=0;i<forecastView.getChildCount();i++){
+      AppCompatImageView dayView = (AppCompatImageView) forecastView.getChildAt(i);
+      dayView.setImageDrawable(mapWeatherToDrawable(forecast.get(i)));
+    }
+  }
+
+  //将选中的状态保存起来，以便转屏之后读取
+  @Override
+  public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+    super.onSaveInstanceState(outState, outPersistentState);
+    outState.putInt(SELECTED_LOCATION_INDEX,mLocationAdapter.getSelectedLocationIndex());
+  }
+}
